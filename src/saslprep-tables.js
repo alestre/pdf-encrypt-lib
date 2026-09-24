54d721c7eb9a3617809ecafa3fe01dd21b409f52
Alexander Strestik <github@ascend-digital.net>
2026-09-24 14:27:36 +0200

    docs: add saslprep MIT notice to derived tables

--- src/saslprep-tables.js
+++ src/saslprep-tables.js
@@ -1,4 +1,30 @@
-﻿// RFC 3454 stringprep tables used by SASLprep (RFC 4013), as sorted inclusive
+﻿// Derived from the saslprep npm package (https://github.com/reklatsmasters/saslprep).
+// Its license notice, as required by the MIT license:
+//
+// Copyright (c) 2014 Dmitry Tsvettsikh
+//
+// Permission is hereby granted, free of charge, to any person
+// obtaining a copy of this software and associated documentation
+// files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use,
+// copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following
+// conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+// OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+// HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+// WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+
+// RFC 3454 stringprep tables used by SASLprep (RFC 4013), as sorted inclusive
 // [start, end, start, end, ...] code point ranges. Generated from the tables of the
 // saslprep npm package (MIT); the unassigned table (A.1) is omitted because
 // preparePassword always allows unassigned code points.
